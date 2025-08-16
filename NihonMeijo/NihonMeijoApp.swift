7bcb5a00e4a1b9636f7705c8068b393e45988ea8
KantoWatanabe <>
2025-08-16 16:51:18 +0900

    first commit

---
+++ NihonMeijo/NihonMeijoApp.swift
@@ -0,0 +1,15 @@
+//
+//  NihonMeijoApp.swift
+//  NihonMeijo
+//
+
+import SwiftUI
+
+@main
+struct NihonMeijoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
